b8359591254c5422a283b7e62ad9e397a9600b9e
Christoffer Kragh Pedersen <chriskpedersen@hotmail.com>
2020-02-05 10:28:57 +0100

    Begyndte test applikation, klar til at blive testet med en telefon

--- App/XamarinFormsApp/XamarinFormsApp/MainPage.xaml.cs
+++ App/XamarinFormsApp/XamarinFormsApp/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +15,29 @@ namespace XamarinFormsApp
   [DesignTimeVisible(false)]
   public partial class MainPage : ContentPage
   {
+    private string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+      "notes.txt");
     public MainPage()
     {
       InitializeComponent();
+
+      if (File.Exists(_fileName))
+      {
+        Editor.Text = File.ReadAllText(_fileName);
+        Debug.WriteLine($"File loaded from path: '{_fileName}' with text '{Editor.Text}'.");
+      }
+    }
+
+    private void SaveButton_OnClicked(object sender, EventArgs e)
+    {
+      File.WriteAllText(_fileName, Editor.Text);
+    }
+
+    private void DeleteButton_OnClicked(object sender, EventArgs e)
+    {
+      if (File.Exists(_fileName))
+        File.Delete(_fileName);
+      Editor.Text = String.Empty;
     }
   }
 }
