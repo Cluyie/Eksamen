7fe8dded06874ce97769bc58438a42ba78b64b65
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2020-02-15 20:54:15 +0100

    Addet a MSUnitets prosjekt Samt tilføjdet Controler metoden

---
+++ Rest/Rest ApiTest/ControlerTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rest_ApiTest
+{
+    [TestClass]
+    public class ControlerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
