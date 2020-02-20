ac135f228f40e1ad87110d18078d1450c4f65ec4
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2020-02-20 11:17:15 +0100

    Lavet metoderne til test af UserController UpdateUser Metoden

---
+++ Rest/Rest ApiTest/UserControlerTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rest_ApiTest
+{
+    [TestClass]
+    public class UserControlerTest
+    {
+        [TestMethod]
+        public void PutUser_OpretUser()
+        {
+            
+        }
+        public void PutUser_NotEnothUserInfo()
+        {
+            
+        }
+    }
+}
