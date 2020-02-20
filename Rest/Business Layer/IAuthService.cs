4583dfd8f0c192a4a8585f5ff69d218563164cf7
Tobbe3108 <Tobbe3108@gmail.com>
2020-02-20 11:51:05 +0100

    Merge branch 'master' into CodeCleanUp

---
+++ Rest/Business Layer/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Layer
+{
+    interface IAuthService
+    {
+        IActionResult Login([FromBody] User user);
+    }
+}
