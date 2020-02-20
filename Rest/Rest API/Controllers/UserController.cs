193def9a7cf387754e4df6c2357de8e03e51b7f8
Tobbe3108 <Tobbe3108@gmail.com>
2020-02-20 11:10:57 +0100

    Controllers and first API method

---
+++ Rest/Rest API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(Guid id, [FromBody] User user)
+        {
+            return Ok(user);
+        }
+    }
+}
