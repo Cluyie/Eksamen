2296e703287c73b44609e63d48815cd98b17620f
Christoffer Kragh Pedersen <chriskpedersen@hotmail.com>
2020-02-21 13:06:10 +0100

    Added register page and a few view models

---
+++ App/XamarinFormsApp/XamarinFormsApp/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsApp.ViewModel
+{
+  public class ProfileViewModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Zipcode { get; set; }
+    public string Country { get; set; }
+  }
+}
