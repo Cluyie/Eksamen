1976e81fb6253caa7775f9b870360ae45ae4a776
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-02-21 13:31:27 +0100

    login

---
+++ App/XamarinFormsApp/XamarinFormsApp/View/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFormsApp.ViewModel;
+
+namespace XamarinFormsApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            //var login = (Login)BindingContext;
+
+            if (!string.IsNullOrWhiteSpace(login.UserName) && !string.IsNullOrWhiteSpace(login.Password))
+            {
+                //send mig til login api
+                //await Navigation.PushAsync(/*hjem eller bruger*/);
+            }
+            else
+            {
+                //else
+            }
+
+            //
+            await Navigation.PopAsync();
+        }
+    }
+}
