4ac5805987dd4062a3531164403f793d93f4273f
simonhanseen <simonhansen031@hotmail.com>
2020-02-21 10:51:06 +0100

    Profile page setup

---
+++ App/XamarinFormsApp/XamarinFormsApp/View/ProfilePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Windows;
+using XamarinFormsApp.ViewModel;
+
+namespace XamarinFormsApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentView
+    {
+        ProfileViewModel _profileViewModel;
+        public ProfilePage()
+        {
+            InitializeComponent();
+            BindingContext = _profileViewModel
+                = new ProfileViewModel();
+        }
+    }
+}
