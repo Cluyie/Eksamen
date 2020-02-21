e62ea776802fc52255b2d0a739bbda32e191f217
simonhanseen <simonhansen031@hotmail.com>
2020-02-21 13:33:03 +0100

    Change ViewModel

--- App/XamarinFormsApp/XamarinFormsApp/Model/Account.cs
+++ App/XamarinFormsApp/XamarinFormsApp/Model/Account.cs
@@ -6,5 +6,8 @@ namespace XamarinFormsApp.Model
 {
     class Account
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
--- App/XamarinFormsApp/XamarinFormsApp/ViewModel/AccountViewModel.cs
+++ App/XamarinFormsApp/XamarinFormsApp/ViewModel/AccountViewModel.cs
@@ -6,8 +6,5 @@ namespace XamarinFormsApp.ViewModel
 {
   public class AccountViewModel
   {
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
   }
 }
