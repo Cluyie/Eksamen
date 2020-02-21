6855a577c3e774304b3cd56ab50084970e6eb1aa
simonhanseen <simonhansen031@hotmail.com>
2020-02-21 13:07:46 +0100

    Layout profile done

--- App/XamarinFormsApp/XamarinFormsApp/View/ProfilePage.xaml.cs
+++ App/XamarinFormsApp/XamarinFormsApp/View/ProfilePage.xaml.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XamarinFormsApp.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Windows;
-using XamarinFormsApp.ViewModel;
 
 namespace XamarinFormsApp.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProfilePage : ContentView
+    public partial class ProfilePage : ContentPage
     {
         ProfileViewModel _profileViewModel;
         public ProfilePage()
@@ -19,6 +18,16 @@ namespace XamarinFormsApp.View
             InitializeComponent();
             BindingContext = _profileViewModel
                 = new ProfileViewModel();
+        }
+
+        private void UpdateButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginSettingsButton_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
