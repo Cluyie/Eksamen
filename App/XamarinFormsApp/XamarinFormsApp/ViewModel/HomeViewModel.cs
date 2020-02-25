cc7785298932bae5dc1e606addb175bebb02cbd9
Christoffer Kragh Pedersen <chriskpedersen@hotmail.com>
2020-02-25 01:22:51 +0100

    Fixed home page error, can't get it to work

--- App/XamarinFormsApp/XamarinFormsApp/ViewModel/HomeViewModel.cs
+++ App/XamarinFormsApp/XamarinFormsApp/ViewModel/HomeViewModel.cs
@@ -19,15 +19,11 @@ namespace XamarinFormsApp.ViewModel
     }
     #endregion
 
-    public string Username
-    { 
-      get 
-      {
-        return $"Velkommen {Username ?? "No username"}!";  //TODO Fjern no username
-      } 
-      set { Username = value; } 
-    }
+    public string Username { get; set; }
 
+    /// <summary>
+    /// Not in use, can't get to work
+    /// </summary>
     private async void SetUsername()
     {
       var homeUser = await _proxy.GetAsync<Home>("User/7166f6e1-7de5-4757-8bc3-26145f991a7b");
