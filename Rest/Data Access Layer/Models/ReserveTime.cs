d9305cb7c16c7a801eae362d9354466fb219986e
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-03-09 11:26:56 +0100

    created reservation model

---
+++ Rest/Data Access Layer/Models/ReserveTime.cs
@@ -0,0 +1,13 @@
+﻿using Data_Access_Layer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Access_Layer.Models
+{
+    public class ReserveTime : IReserveTime
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
