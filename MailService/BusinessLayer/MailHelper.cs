077db511b2d3488ccb5f9dd86c7366e274f2bb7a
Christoffer Kragh Pedersen <chriskpedersen@hotmail.com>
2020-03-10 23:40:54 +0100

    Example of a MailService

---
+++ MailService/BusinessLayer/MailHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Mail;
+using Models.Mail;
+
+namespace BusinessLayer
+{
+  public class MailHelper
+  {
+    public void SendMail(string title, string htmlBody, string recipient, string[] ccRecipients = null)
+    {
+      SmtpClient smtpClient = new SmtpClient(Properties.Resources.MailService_HostName, int.Parse(Properties.Resources.MailService_HostPort)); //"laraSMTP", 25);
+
+      smtpClient.Credentials = new System.Net.NetworkCredential(Properties.Resources.MailService_Username, Properties.Resources.MailService_Password);
+      // smtpClient.UseDefaultCredentials = true; // uncomment if you don't want to use the network credentials
+      smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+      smtpClient.EnableSsl = true;
+
+      //Setting From, subject and body
+      MailMessage mail = new MailMessage
+      {
+        From = new MailAddress(Properties.Resources.MailService_SenderEmail), // "booking@ucl.dk");
+        Subject = title,
+        Body = htmlBody,
+      };
+
+      //Setting To and CC
+      mail.To.Add(new MailAddress(recipient));
+      foreach (string ccRecipient in ccRecipients)
+      {
+        mail.CC.Add(new MailAddress(ccRecipient));
+      }
+
+      smtpClient.Send(mail);
+    }
+
+    public void SendMail(MailContent content)
+    {
+      SmtpClient smtpClient = new SmtpClient(Properties.Resources.MailService_HostName,
+        int.Parse(Properties.Resources.MailService_HostPort))
+      {
+        Credentials = new System.Net.NetworkCredential(Properties.Resources.MailService_Username,
+          Properties.Resources.MailService_Password),
+        DeliveryMethod = SmtpDeliveryMethod.Network,
+        EnableSsl = true
+      }; //"laraSMTP", 25);
+
+      // smtpClient.UseDefaultCredentials = true; // uncomment if you don't want to use the network credentials
+
+      //Setting From, subject and body
+      MailMessage mail = new MailMessage
+      {
+        From = new MailAddress(Properties.Resources.MailService_SenderEmail), // "booking@ucl.dk");
+        Subject = content.TitleContent,
+        Body = content.BodyContent,
+      };
+
+      //Setting To and CC
+      mail.To.Add(new MailAddress(content.Recipient));
+      foreach (string ccRecipient in content.CcRecipients)
+      {
+        mail.CC.Add(new MailAddress(ccRecipient));
+      }
+
+
+      smtpClient.Send(mail);
+    }
+  }
+}
--- MailService/MailService/Controllers/MailController.cs
+++ MailService/MailService/Controllers/MailController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using BusinessLayer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Models;
+using Models.Mail;
+using UCLToolBox;
 
 namespace MailService.Controllers
 {
@@ -13,15 +17,31 @@ namespace MailService.Controllers
   public class MailController : ControllerBase
   {
     private readonly ILogger<MailController> _logger;
+    private ApiClientProxy _proxy;
+    private MailHelper _mailHelper;
 
-    public MailController(ILogger<MailController> logger)
+    public MailController(ILogger<MailController> logger, ApiClientProxy proxy, MailHelper mailHelper)
     {
       _logger = logger;
+      _proxy = proxy;
+      _mailHelper = mailHelper;
     }
 
     [HttpPost]
-    public ApiResponse<string> PostMail()
+    public ApiResponse<string> PostMail(int recipientId, [FromBody] MailContent content)
     {
+      try
+      {
+        var user = _proxy.Get<User>("User/GetProfileById/" + recipientId);
+        content.Recipient = user.Email;
+        _mailHelper.SendMail(content);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        throw;
+        return new ApiResponse<string>(ApiResponseCode.InternalServerError, e.Message);
+      }
       return new ApiResponse<string>(ApiResponseCode.OK, "Email send");
     }
   }
