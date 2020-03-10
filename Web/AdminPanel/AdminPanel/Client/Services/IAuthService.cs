d7318b95a95375610920d029d39f41858c7fcad9
Skitsine <49900464+Skitsine@users.noreply.github.com>
2020-03-10 12:28:05 +0100

    Create IAuthService.cs

---
+++ Web/AdminPanel/AdminPanel/Client/Services/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdminPanel.Shared;
+
+namespace AdminPanel.Client.Services
+{
+    interface IAuthService
+    {
+        Task Login(LoginDTO loginDTO);
+
+        Task Logout();
+
+        Task<bool> IsAuthenticated();
+    }
+}
