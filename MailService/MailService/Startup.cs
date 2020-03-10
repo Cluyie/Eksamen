b8764785df9110a096698b96998cb73abfb05574
Christoffer Kragh Pedersen <chriskpedersen@hotmail.com>
2020-03-10 11:26:14 +0100

    Added mailservice and swashbuckle

--- MailService/MailService/Startup.cs
+++ MailService/MailService/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MailService
 {
@@ -26,6 +27,10 @@ namespace MailService
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +41,16 @@ namespace MailService
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseHttpsRedirection();
+      //app.UseHttpsRedirection();
+
+      app.UseSwagger();
+
+      // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+      // specifying the Swagger JSON endpoint.
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+      });
 
       app.UseRouting();
 
