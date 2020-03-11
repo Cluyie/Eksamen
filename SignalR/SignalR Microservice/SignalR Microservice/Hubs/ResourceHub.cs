8f5d116d88e775984fae1b2ec2d9b7ac95b89b43
Tobbe3108 <Tobbe3108@gmail.com>
2020-03-11 13:00:49 +0100

    Update ResourceHub.cs

--- SignalR/SignalR Microservice/SignalR Microservice/Hubs/ResourceHub.cs
+++ SignalR/SignalR Microservice/SignalR Microservice/Hubs/ResourceHub.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-<<<<<<< Updated upstream
-=======
 using Models;
->>>>>>> Stashed changes
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +10,13 @@ namespace SignalR_Microservice.Hubs
 {
     public class ResourceHub : Hub
     {
-        //Change object to Reservation type when it is available from DB team
-<<<<<<< Updated upstream
-        public async Task UpdateReservation(object reservation)
-=======
         public async Task UpdateReservation(Reservation reservation)
->>>>>>> Stashed changes
         {
             await Clients.All.SendAsync("UpdateReservation", reservation);
 
 
             //SignalR Client methods for UpdateReservation
-<<<<<<< Updated upstream
-            //_hubConnection.On<object>("UpdateReservation", (reservation) =>
-=======
             //_hubConnection.On<Reservation>("UpdateReservation", (reservation) =>
->>>>>>> Stashed changes
             //        {
             //            if (resource.Reservations.Find(r => r.Id = reservation.Id) != null)
             //	        {
@@ -46,21 +34,13 @@ namespace SignalR_Microservice.Hubs
         }
 
         //Change object to Reservation type when it is available from DB team
-<<<<<<< Updated upstream
-        public async Task UpdateResource(object resource)
-=======
         public async Task UpdateResource(Resource resource)
->>>>>>> Stashed changes
         {
             await Clients.All.SendAsync("UpdateResource", resource);
 
 
             //SignalR Client methods for UpdateResource
-<<<<<<< Updated upstream
-            //_hubConnection.On<object>("UpdateResource", (resource) =>
-=======
             //_hubConnection.On<Resource>("UpdateResource", (resource) =>
->>>>>>> Stashed changes
             //        {
             //            if (resourceList.Find(r => r.Id == resource.Id) != null)
             //            {
