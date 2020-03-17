9ae6a800e43ab223d3062eac2272dad73708e028
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-03-17 17:23:43 +0100

    swagger added

--- Rest/Rest API/Startup.cs
+++ Rest/Rest API/Startup.cs
@@ -15,60 +15,76 @@ using Microsoft.Extensions.Logging;
 using Rest_API.Middleware;
 using AutoMapper;
 using Data_Access_Layer.Models;
+using Microsoft.OpenApi.Models;
 
 
 namespace Rest_API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<IdentityContext>();
+            services.AddScoped<ApplicationContext>();
+            services.AddScoped<AuthService>();
+            services.AddScoped<UserService>();
+            services.AddScoped<ResourceService>();
+            services.AddScoped<ReservationService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            //AutoMapper setup
+
+            Mapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, User>()
+       .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            }));
+            services.AddSingleton(mapper);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseTokenValidation();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddControllers();
-      services.AddScoped<IdentityContext>();
-      services.AddScoped<ApplicationContext>();
-      services.AddScoped<AuthService>();
-      services.AddScoped<UserService>();
-      services.AddScoped<ResourceService>();
-      services.AddScoped<ReservationService>();
-
-      //AutoMapper setup
-
-      Mapper mapper = new Mapper(new MapperConfiguration(cfg =>
-      {
-        cfg.CreateMap<User, User>()
-     .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
-      }));
-      services.AddSingleton(mapper);
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      //app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseTokenValidation();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
 }
