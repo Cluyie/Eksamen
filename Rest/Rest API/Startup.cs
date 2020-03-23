f12812506a13ea9adb4fee74f98ecddb3a7e3a69
Caspar Hougaard <>
2020-03-23 10:41:22 +0100

    Added ../ to the swagger endpoint. Fixes swagger on the ISS.

--- Rest/Rest API/Startup.cs
+++ Rest/Rest API/Startup.cs
@@ -72,7 +72,7 @@ namespace Rest_API
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "My API V1");
             });
 
             //app.UseHttpsRedirection();
