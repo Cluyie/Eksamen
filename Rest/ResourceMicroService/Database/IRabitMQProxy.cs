a3d68b02feba37f96ef34ca9ae767bcda325265a
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2020-04-05 14:23:06 +0200

    Interface for RbitMQ

---
+++ Rest/ResourceMicroService/Database/IRabitMQProxy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceMicroService.Database
+{
+    interface IRabitMQProxy
+    {
+        public async Task NewResrource(IResource resource);
+        public async Task UpdatereResrource(IResource resource);
+        public async Task ResrourceDelte(Guid resourceId);
+    }
+}
