404f3371754f3d106f02ff964a7c6dec5a556f1a
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2020-04-06 05:46:22 +0200

    Addet DatabaseContext to Microservicen samt Interfaces

---
+++ Rest/ResourceMicrosDtabase/Models/Resource.cs
@@ -0,0 +1,15 @@
+﻿using ResourcInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceMicrosDtabase.Models
+{
+    public class Resource : IResource<AvaiableTime>
+    {
+        public Guid Id { get ; set ; }
+        public string Name { get ; set ; }
+        public string Description { get ; set ; }
+        public List<AvaiableTime> Avaiabletimes { get ; set ; }
+    }
+}
