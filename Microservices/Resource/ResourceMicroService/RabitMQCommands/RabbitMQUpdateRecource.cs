3714ef91545fb09d816044b64dda07a73431036f
Christian Sten Jørgensen <chri206z@edu.ucl.dk>
2020-04-14 14:20:12 +0200

    Started på RabbitMQ

---
+++ Microservices/Resource/ResourceMicroService/RabitMQCommands/RabbitMQUpdateRecource.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Bus.Commands;
+
+namespace ResourceMicroService.RabitMQCommands
+{
+    public class RabbitMQUpdateRecource : Command
+    {
+
+        public Resource InputResource { get; set; }
+        public Models.Resource ResourceToChange { get; set; }
+        public RabbitMQUpdateRecource()
+        {
+
+        }
+    }
+}
