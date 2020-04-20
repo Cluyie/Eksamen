b2680583ac721c6246b1aa2f37a16567df828b18
Henrik Langer Hallenberg Schou <henr758c@edu.ucl.dk>
2020-04-20 12:59:35 +0200

    Finished Controler

--- Microservices/Ticket/UCLDreamTeam.Ticket.Api/Controler/TicketController.cs
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Api/Controler/TicketController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UCLDreamTeam.Ticket.Domain.Models;
+using UCLDreamTeam.Ticket.Application.Services;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,18 +15,24 @@ namespace UCLDreamTeam.Ticket.Api
     [ApiController]
     public class TicketController : ControllerBase
     {
+        TicketService TicketService { get; set; }
+        public TicketController(TicketService ticketService)
+        {
+            TicketService = ticketService;
+        }
         // GET: <controller>
         [HttpGet("{TickedId}")]
-        public IEnumerable<string> GetTicked(Guid TickedId)
+        public async Task<Domain.Models.Ticket> GetTicket(Guid TickedId)
         {
-            return new List<string>{ "test"};
+
+            return await TicketService.GetByIdAsync(TickedId);
         }
 
         // GET api/<controller>/5
         [HttpGet("User/{UserId}")]
-        public string GetUserTicked(Guid UserId)
+        public async Task<IEnumerable<Guid>> GetUserTicket(Guid UserId)
         {
-            return "test2";
+            return (await TicketService.GetByUserIdAsync(UserId)).ToList().Select(T => T.Id);
         }
     }
 }
