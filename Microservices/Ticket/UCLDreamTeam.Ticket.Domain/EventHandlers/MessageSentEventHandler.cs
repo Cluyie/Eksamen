a8cf6315f2bb591feb766f4d7c021f486132ee9e
Tobbe3108 <Tobbe3108@gmail.com>
2020-04-20 11:20:28 +0200

    initial event handlers

---
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Domain/EventHandlers/MessageSentEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Bus.Events;
+
+namespace UCLDreamTeam.Ticket.Domain.EventHandlers
+{
+    public class MessageSentEventHandler : Event
+    {
+    }
+}
---
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Domain/Events/MessageSentEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCLDreamTeam.Ticket.Domain.Models;
+
+namespace UCLDreamTeam.Ticket.Domain.Events
+{
+    public class MessageSentEvent
+    {
+        public Message Message { get; set; }
+
+        public MessageSentEvent(Message message)
+        {
+            Message = message;
+        }
+
+    }
+}
