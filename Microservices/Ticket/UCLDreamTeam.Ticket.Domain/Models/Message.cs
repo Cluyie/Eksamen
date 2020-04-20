d7dba207627a195c882c2dfc812542ecd4d36f72
Tobbe3108 <Tobbe3108@gmail.com>
2020-04-20 11:13:07 +0200

    Models

---
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Domain/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UCLDreamTeam.Ticket.Domain.Models
+{
+    public class Message : IMessage
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Seen { get; set; } = false;
+    }
+}
---
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Domain/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Models.Interfaces;
+
+namespace UCLDreamTeam.Ticket.Domain.Models
+{
+    public class Ticket : ITicket<Message>
+    {
+        public Guid Id { get; set; }
+        public bool? Active { get; set; }
+        public string Name { get; set; }
+        public Status Status { get; set; }
+        public List<Message> Messages { get; set; }
+        public Guid ReservationId { get; set; }
+    }
+}
