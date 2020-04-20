8f9c2d9ee61faf651cfa1a6103aaed48edf060d5
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-04-20 12:49:50 +0200

    Hahha jeg tilføjer en abstrakt klasse startup >:))))))))

--- Microservices/Ticket/UCLDreamTeam.Ticket.Api/StartupDevelopment.cs
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Api/StartupDevelopment.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using UCLDreamTeam.Ticket.Data.Contexts;
 
 namespace UCLDreamTeam.Ticket.Api
 {
-    public class Startup : StartupAbstract
+    public class StartupDevelopment : StartupAbstract
     {
-        public Startup(IConfiguration configuration) : base(configuration)
+        public StartupDevelopment(IConfiguration configuration) : base(configuration)
         {
         }
 
@@ -19,7 +24,7 @@ namespace UCLDreamTeam.Ticket.Api
         public override void ConfigureServices(IServiceCollection services)
         {
             base.ConfigureServices(services);
-            services.AddDbContext<TicketDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TicketDbConnection")));
+            services.AddDbContext<TicketDbContext>(options => options.UseInMemoryDatabase("TicketDb"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
