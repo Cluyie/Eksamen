cc6479410fea5a775ab839b714be03dde327cff4
SkrumpelNissen <casparhougaard@gmail.com>
2020-04-20 14:53:40 +0200

    Added role based authorization to the customer service chat page.
    Roles are CasE SensItiVe. Still needs role based authorization else where.

--- Presentation/Web/AdminPanel/Client/Services/CustomAuthStateProvider.cs
+++ Presentation/Web/AdminPanel/Client/Services/CustomAuthStateProvider.cs
@@ -1,6 +1,10 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System;
+using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.Linq;
 
 namespace AdminPanel.Client.Services
 {
@@ -23,13 +27,12 @@ namespace AdminPanel.Client.Services
             if (!_credentialsKeeper.HasCredentials())
                 return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
 
-            var identity = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, _credentialsKeeper.Username)
-            }, "ApiWebAuth");
+            IEnumerable < Claim > claims = ParseClaimsFromJwt(_credentialsKeeper.Token);
 
-            var user = new ClaimsPrincipal(identity);
+            //Add username to the claims
+            claims.Append(new Claim(ClaimTypes.Name, _credentialsKeeper.Username));
 
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "ApiWebAuth"));
             return Task.FromResult(new AuthenticationState(user));
         }
 
@@ -40,6 +43,48 @@ namespace AdminPanel.Client.Services
         public void Refresh()
         {
             NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+            if (roles != null)
+            {
+                if (roles.ToString().Trim().StartsWith("["))
+                {
+                    var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+
+                    foreach (var parsedRole in parsedRoles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                    }
+                }
+                else
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+                }
+
+                keyValuePairs.Remove(ClaimTypes.Role);
+            }
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+
+            return claims;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
         }
     }
 }
