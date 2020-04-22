0893fb80ef7c6dc8c38463d8740d03768d3b91df
Tobbe3108 <Tobbe3108@gmail.com>
2020-04-22 13:56:32 +0200

    Fixed mail Service

--- Microservices/MailService/UCLDreamTeam.Mail.Domain/Events/UserDeletedEvent.cs
+++ Microservices/MailService/UCLDreamTeam.Mail.Domain/Events/UserDeletedEvent.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RabbitMQ.Bus.Events;
+using UCLDreamTeam.Mail.Domain.Models;
 using UCLDreamTeam.SharedInterfaces.Interfaces;
 
 namespace UCLDreamTeam.Mail.Domain.Events
