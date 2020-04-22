b027418a780291f1a0499ad800325e156c2ef7b9
Tobbe3108 <Tobbe3108@gmail.com>
2020-04-22 15:25:25 +0200

    Model interfaces for ticket and message

---
+++ Core/Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCLDreamTeam.SharedInterfaces
+{
+    public enum Status
+    {
+        Active,
+        Waiting,
+        Closed
+    }
+}
