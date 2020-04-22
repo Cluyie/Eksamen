5179c7f6f1695056bbbe83df12a0feadb9055573
SkrumpelNissen <casparhougaard@gmail.com>
2020-04-22 22:31:00 +0200

    Added the reservation service, and Interface for it. Where should the data go now?

---
+++ Presentation/Web/AdminPanel/Client/Services/IReservationService.cs
@@ -0,0 +1,13 @@
+﻿using AdminPanel.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Client.Services
+{
+    public interface IReservationService
+    {
+        Task<Reservation> GetFromId(Guid id);
+    }
+}
