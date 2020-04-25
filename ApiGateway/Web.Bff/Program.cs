2fcec2758512cf2efa62ccd5d63f482dda70b13c
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-04-26 01:19:51 +0200

    Changed downstream path on ocelot files so that it work on the server. Does not work locally, need to fix somehow

--- ApiGateway/Web.Bff/Program.cs
+++ ApiGateway/Web.Bff/Program.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.Middleware;
 using Ocelot.DependencyInjection;
-using System.IO;
-using Microsoft.AspNetCore;
 
 namespace Web.Bff
 {
