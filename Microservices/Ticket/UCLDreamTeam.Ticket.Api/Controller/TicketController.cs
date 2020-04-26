ce162d84eae480f4a2f327d5d411e1a3387da6c0
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-04-26 11:42:02 +0200

    Controller renaming

--- Microservices/Ticket/UCLDreamTeam.Ticket.Api/Controller/TicketController.cs
+++ Microservices/Ticket/UCLDreamTeam.Ticket.Api/Controller/TicketController.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using UCLDreamTeam.Ticket.Domain.Models;
-using UCLDreamTeam.Ticket.Application.Services;
-using System.Linq;
 using UCLDreamTeam.SharedInterfaces.Interfaces;
+using UCLDreamTeam.Ticket.Application.Services;
+using UCLDreamTeam.Ticket.Domain.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace UCLDreamTeam.Ticket.Api
+namespace UCLDreamTeam.Ticket.Api.Controller
 {
     [Route("[controller]")]
     [ApiController]
