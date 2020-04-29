88bec132013a9a0061bf5aacb339d9295d41c94a
Christian Sten Jørgensen <chri206z@edu.ucl.dk>
2020-04-29 10:42:52 +0200

    Queue design

---
+++ Presentation/App/XamarinFormsApp/XamarinFormsApp/View/QueuePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFormsApp.Model;
+using XamarinFormsApp.ViewModel;
+
+namespace XamarinFormsApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QueuePage : ContentPage
+    {
+        public QueuePage()
+        {
+            InitializeComponent();
+
+        }
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
